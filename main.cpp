089e011737dd08fee39a70e09748d4c9f869f0ef
Seçkin Poyraz <firefrost33@gmail.com>
2020-05-01 06:23:06 +0300

    added descriptions

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 int main(){
     
     while(true){
+        //create random tables continiously on the heap
         Minesweeper *app = new Minesweeper(30,16,55);
         delete app;
         cout << endl;
-        Sleep( 3000 );
+        Sleep( 3000 ); //wait for 3 sec
     }
     
     return 0;
